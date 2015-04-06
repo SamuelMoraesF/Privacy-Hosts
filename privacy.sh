6cb17b8dddbf71b416850649bad9a94d2dc140e8
SamuelMoraesF <samuel@samuelmoraes.com>
2015-04-06 18:39:40 -0300

    End hosts file

--- privacy.sh
+++ privacy.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root" 1>&2
-    exit 1
-else
-    echo "Backing Up..." 1>&2
-
-	if [ -f /etc/hosts.bkp ]; then
-   		rm -r /etc/hosts.bkp
-	fi
- 
-	cp /etc/hosts /etc/hosts.bkp
-
-	echo 'Getting blocked services...'
+	echo 'Getting files...'
 	echo ''
 	echo 'Getting (1/6)'
 	echo ''
-	curl "http://winhelp2002.mvps.org/hosts.txt" >> /etc/hosts
+	curl "http://winhelp2002.mvps.org/hosts.txt" >> winhelp2002
 	echo ''
 	echo 'Getting (2/6)'
 	echo ''
-	curl "http://someonewhocares.org/hosts/zero/hosts" >> /etc/hosts
+	curl "http://someonewhocares.org/hosts/zero/hosts" >> someonewhocares
 	echo ''
 	echo 'Getting (3/6)'
 	echo ''
-	curl "http://jansal.googlecode.com/svn/trunk/adblock/hosts" >> /etc/hosts
+	curl "http://jansal.googlecode.com/svn/trunk/adblock/hosts" >> jansal
 	echo ''
 	echo 'Getting (4/6)'
 	echo ''
-	curl "http://adblock.gjtech.net/?format=hostfile" >> /etc/hosts
+	curl "http://adblock.gjtech.net/?format=hostfile" >> adblock
 	echo ''
 	echo 'Getting (5/6)'
 	echo ''
-	curl "http://www.hostsfile.org/Downloads/hosts.txt" >> /etc/hosts
+	curl "http://www.hostsfile.org/Downloads/hosts.txt" >> hostsfile
 	echo ''
 	echo 'Getting (6/6)'
 	echo ''
-	curl "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" >> /etc/hosts
+	curl "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" >> pgl-yoyo
 	echo ''
 	echo 'All Files are downloaded'
 	echo ''
